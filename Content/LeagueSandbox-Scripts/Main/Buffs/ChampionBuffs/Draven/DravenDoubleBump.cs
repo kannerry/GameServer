c878a9030b21263b74dde12e9ab7e001cc831efe
kannerry <46495718+kannerry@users.noreply.github.com>
2021-12-24 19:37:53 +0000

    Draven + more
    LEAGUE OF DRAVEN!!!
    
    - killing minions does not add a stack
    - auto attacks before pressing q do not have fx
    - e is a knockup, not knock-aside
    - r recast has a cast time
    - q looks wierd
    
    ============
    ALSO
    remove spacebar in "Jarvan IV" folder
    add TwistedFate folder name :weary:

---
+++ Content/LeagueSandbox-Scripts/Main/Buffs/ChampionBuffs/Draven/DravenDoubleBump.cs
@@ -0,0 +1,40 @@
+﻿using GameServerCore.Domain.GameObjects;
+using GameServerCore.Domain.GameObjects.Spell;
+using GameServerCore.Enums;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.Stats;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+
+namespace Buffs
+{
+    internal class DravenDoubleBump : IBuffGameScript
+    {
+        public BuffType BuffType => BuffType.SLOW;
+        public BuffAddType BuffAddType => BuffAddType.RENEW_EXISTING;
+        public int MaxStacks => 1;
+        public bool IsHidden => false;
+
+        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        private IParticle highlander;
+
+        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            var owner = ownerSpell.CastInfo.Owner;
+
+            StatsModifier.MoveSpeed.PercentBonus = StatsModifier.MoveSpeed.PercentBonus - (15f + ownerSpell.CastInfo.SpellLevel * 5) / 100f;
+            //StatsModifier.AttackSpeed.PercentBonus = StatsModifier.AttackSpeed.PercentBonus + (5f + ownerSpell.CastInfo.SpellLevel * 25) / 100f;
+            unit.AddStatModifier(StatsModifier);
+            // TODO: add immunity to slows
+        }
+
+        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            RemoveParticle(highlander);
+        }
+
+        public void OnUpdate(float diff)
+        {
+        }
+    }
+}
---
+++ Content/LeagueSandbox-Scripts/Main/Buffs/ChampionBuffs/Draven/DravenPassive.cs
@@ -0,0 +1,33 @@
+﻿using GameServerCore.Domain.GameObjects;
+using GameServerCore.Domain.GameObjects.Spell;
+using GameServerCore.Enums;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.Stats;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+
+namespace Buffs
+{
+    internal class DravenPassiveStacks : IBuffGameScript
+    {
+        public BuffType BuffType => BuffType.COMBAT_ENCHANCER;
+        public BuffAddType BuffAddType => BuffAddType.STACKS_AND_RENEWS;
+        public int MaxStacks => 255;
+        public bool IsHidden => false;
+
+        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        private IParticle highlander;
+
+        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+        }
+
+        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+        }
+
+        public void OnUpdate(float diff)
+        {
+        }
+    }
+}
